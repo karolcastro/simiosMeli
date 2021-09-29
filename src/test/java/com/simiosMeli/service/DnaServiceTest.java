aa559c885f7412f888d2e3c2ddec3d94641b6cf7
karol castro <karolinycosta@yahoo.com.br>
2021-09-29 18:30:18 -0300

    adicionando testes DnaServiceTest

--- src/test/java/com/simiosMeli/service/DnaServiceTest.java
+++ src/test/java/com/simiosMeli/service/DnaServiceTest.java
@@ -9,38 +9,38 @@ public class DnaServiceTest {
 
     @Test
     public void shouldReturnTrueWhenHasDnaInHorizontal() {
-        String[] dna = new String[]{"CAGTAG", "TTTTAT", "GCAGGC", "ACTGAC", "TGAATC"};
+        String[] dnaSimios = new String[]{"CAGTAG", "TTTTAT", "GCAGGC", "ACTGAC", "TGAATC"};
         Assert.assertTrue(this.dnaService.isSimian(dnaSimios));
 
     }
 
     @Test
     public void shouldReturnTrueWhenHasDnaInVertical() {
-        String[] dna = new String[]{"CAGTAG", "TATTAT", "GAAGGC", "AATGAC", "TGAATC", "TGAATC"};
-        Assert.assertTrue(this.dna.isSimion(dna));
+        String[] dnaSimios = new String[]{"CAGTAG", "TATTAT", "GAAGGC", "AATGAC", "TGAATC", "TGAATC"};
+        Assert.assertTrue(this.dnaService.isSimian(dnaSimios));
     }
 
     @Test
     public void shouldReturnTrueWhenHasDnaInDiagonalPrincipalParaBaixo() {
-        String[] dna = new String[]{"TGTAG", "ATGGC", "GAAGA", "GGATG", "TGAAT"};
-        Assert.assertTrue(this.dna.isSimion(dna));
+        String[] dnaSimios = new String[]{"TGTAG", "ATGGC", "GAAGA", "GGATG", "TGAAT"};
+        Assert.assertTrue(this.dnaService.isSimian(dnaSimios));
     }
 
     @Test
     public void shouldReturnTrueWhenHasDnaInDiagonalPrincipalParaCima() {
-        String[] dna = new String[]{"TGTGG", "ATGGG", "GGAGA", "GGATG", "TGAAT"};
-        Assert.assertFalse(this.dna.isSimion(dna));
+        String[] dnaSimios = new String[]{"TGTGG", "ATGGG", "GGAGA", "GGATG", "TGAAT"};
+        Assert.assertFalse(this.dnaService.isSimian(dnaSimios));
     }
 
     @Test
     public void shouldReturnTrueWhenHasDnaInDiagonalSecundariaParaCimaEsquerda() {
-        String[] dna = new String[]{"TGTAG", "ATGTC", "GATGA", "GGATG", "TGTAT"};
-        Assert.assertTrue(this.dna.isSimion(dna));
+        String[] dnaSimios = new String[]{"TGTAG", "ATGTC", "GATGA", "GGATG", "TGTAT"};
+        Assert.assertTrue(this.dnaService.isSimian(dnaSimios));
     }
 
     @Test
     public void shouldReturnTrueWhenHasDnaInDiagonalSecundariaParaCimaDireita() {
-        String[] dna = new String[]{"TGTAG", "ATGTC", "GATGA", "GGATG", "TGTAT"};
-        Assert.assertTrue(this.dna.isSimion(dna));
+        String[] dnaSimios = new String[]{"TGTAG", "ATGTC", "GATGA", "GGATG", "TGTAT"};
+        Assert.assertTrue(this.dnaService.isSimian(dnaSimios));
     }
 }
